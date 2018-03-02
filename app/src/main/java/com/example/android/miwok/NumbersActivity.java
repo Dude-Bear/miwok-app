93f27f2e534c7238f15f1ce41446cdcf287078c5
Dude-Bear <30493095+Dude-Bear@users.noreply.github.com>
2018-03-02 12:35:05 +0100

    - changed the while loop into a for loop

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,8 +1,7 @@
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -35,12 +34,12 @@ public class NumbersActivity extends AppCompatActivity {
         // Find the root view so we can add child views to it
         LinearLayout rootView = (LinearLayout) findViewById(R.id.rootView);
 
-        // Create a variable to keep track of the current index position
-        int index = 0;
 
         // Keep looping until we've reached the end of the list (which means keep looping
-        // as long as the current index position is less than the length of the list)
-        while (index < words.size()) {
+        // as long as the current index position is less than the length of the list).
+        // The index variable keeps track of our current position in the list and
+        // increments by 1 each time the code in the loop is executed.
+        for (int index = 0; index < words.size(); index++) {
             // Create a new TextView
             TextView wordView = new TextView(this);
 
@@ -49,9 +48,6 @@ public class NumbersActivity extends AppCompatActivity {
 
             // Add this TextView as another child to the root view of this layout
             rootView.addView(wordView);
-
-            // Increment the index variable by 1
-            index++;
         }
     }
 }
